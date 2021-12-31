95ea15453d94fb61859ceb874ba9b1aff19a7f2c
Sx-Cheats <65744142+Sx-Cheats@users.noreply.github.com>
2021-12-31 14:24:21 +0100

    Update nasm_ascii.asm

--- nasm_ascii.asm
+++ nasm_ascii.asm
@@ -13,14 +13,14 @@ _start:
 _loop:
     cmp rbx, 127 ; compare esi to 127 (max char in ascii)
     jae _exit ; >=
-    mov [_testt],rbx ; copie esi to _testt, [] -> ref to address _testt
-    inc rbx ;increment esi, esi+1
+    mov [_testt],rbx ; copie rbx to _testt, [] -> ref to address _testt
+    inc rbx ;increment rbx, rbx+1
     jmp _print ;call _print
    
  
 _print:
     mov rdi, 1 ; output
-    mov rsi, _testt ;set const char* to ecx
+    mov rsi, _testt ;set const char* to rsi
     mov rdx, 4 ; set size of char (_testt)
     mov rax, 1 ; call sys_write
     syscall  ;call kernel 
